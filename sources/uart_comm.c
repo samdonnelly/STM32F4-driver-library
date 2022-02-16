de81c3899ae1ea312542299cae0bdca825203b5a
Sam Donnelly <samueldonnelly11@gmail.com>
2022-02-15 20:48:35 -0800

    Begun developing UART driver.

--- sources/uart_comm.c
+++ sources/uart_comm.c
@@ -19,3 +19,32 @@
 
 //=======================================================================================
 
+/*
+ * STM32F4 Reference Manual Procedure to Configuring UART
+ * 
+ * 1. Enable the UART CLOCK and GPIO CLOCK 
+ * 2. Configure the UART pins for alternative functions 
+ * 3. Enable the USART by writing the UE bit in USART_CR1 register to 1
+ *          a. USART_CR1: Control register 1 - Page 550 
+ * 4. Program the M bit in USART_CR1 to define the word length 
+ * 5. Program the number of stop bits in USART_CR2
+ * 6. Select the desired baud rate using the USART_BRR register 
+ * 7. Set the TE bit in USART_CR1 to send an idle frame as first transmission
+ * 8. Enable the transmitter/reciever by setting the TE and RE bits in USART_CR1 register 
+ * 
+ */
+
+void uart_init(void)
+{
+    // UART setup 
+
+    // 1. Enable the UART CLOCK and GPIO CLOCK
+    RCC->APB1ENR |= (SET_BIT << SHIFT_17);  // Enable UART2 Clock 
+    RCC->APB1ENR |= (SET_BIT << SHIFT_0);   // Enable GPIOA clock for TX and RX pins
+
+    // 2. Configure the UART pins for alternative functions 
+ 
+}
+
+
+
