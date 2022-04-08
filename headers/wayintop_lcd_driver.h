aa6dcc1c7c7296fa40d7fc84690ecc2024bd28e6
Sam Donnelly <samueldonnelly11@gmail.com>
2022-04-07 21:34:14 -0600

    Finished LCD driver. Added a clear function and the ability to choose the cursor location. Tested the functionality in the driver test.

--- headers/wayintop_lcd_driver.h
+++ headers/wayintop_lcd_driver.h
@@ -3,7 +3,7 @@
  * 
  * @author Sam Donnelly (samueldonnelly11@gmail.com)
  * 
- * @brief 
+ * @brief Wayintop 2004 LCD screen (HD44780U + PCF8574) driver
  * 
  * @version 0.1
  * @date 2022-02-11
@@ -26,7 +26,6 @@
 // Drivers 
 #include "timers.h"
 #include "i2c_comm.h"
-#include "uart_comm.h"
 
 //=======================================================================================
 
@@ -35,6 +34,7 @@
 // Macros
 
 #define HD44780U_MSG_PER_CMD 4  // Number of I2C bytes sent per one LCD screen command
+#define HD44780U_NUM_CHAR 80    // Number of character spaces on the display 
 
 //=======================================================================================
 
@@ -44,6 +44,13 @@
 
 /**
  * @brief PCF8574 write addresses
+ * 
+ * @details The PCF8574 is the I2C module that relays I2C messages from the controller 
+ *          to the screen. The board has contacts on the surface that allow the address
+ *          to be set manually by grounding certain contacts. The contacts are not 
+ *          grounded by default. These are the addresses used for writing to the 
+ *          screen and the address depends on the contact grounding configuration. 
+ *          These addresses are from the user manual for the I2C module. 
  * 
  */
 typedef enum {
@@ -60,6 +67,13 @@ typedef enum {
 /**
  * @brief PCF8574 read addresses
  * 
+ * @details The PCF8574 is the I2C module that relays I2C messages from the controller 
+ *          to the screen. The board has contacts on the surface that allow the address
+ *          to be set manually by grounding certain contacts. The contacts are not 
+ *          grounded by default. These are the addresses used for reading from the 
+ *          screen and the address depends on the contact grounding configuration. 
+ *          These addresses are from the user manual for the I2C module. 
+ * 
  */
 typedef enum {
     PCF8574_LLL_READ_ADDRESS = 0x41,
@@ -75,6 +89,10 @@ typedef enum {
 /**
  * @brief HD44780U delays 
  * 
+ * @details The screen requires certain delays between each instruction initialization 
+ *          command. These come from the user manaul. The timers driver is used to 
+ *          provide a delay. 
+ * 
  */
 typedef enum {
     HD44780U_DELAY_001MS = 1,
@@ -86,6 +104,9 @@ typedef enum {
 
 /**
  * @brief HD44780U setup commands
+ * 
+ * @details The screen requires certain instructions to be sent to it in order to 
+ *          initialize it correctly. These are directly from the user manual. 
  * 
  */
 typedef enum {
@@ -101,7 +122,15 @@ typedef enum {
 /**
  * @brief HD44780U configuration commands
  * 
- * @details 
+ * @details These commands provide values for the screen backlight, enable or start
+ *          transmission signal, read/write command and register choice. These 
+ *          commands are automatically added to the instruction and data commands 
+ *          in the send functions. See the screen manual for more information. 
+ *          
+ *          bit 3: backlight -> 0 = off, 1 = on
+ *          bit 2: Enable -> 0 = start, 1 = stop
+ *          bit 1: R/W -> 0 = read, 1 = write
+ *          bit 0: RS register -> 0 = instruction, 1 = data
  * 
  */
 typedef enum {
@@ -111,6 +140,24 @@ typedef enum {
     HD44780U_CONFIG_CMD_0X0D = 0x0D
 } hd44780u_config_cmds_t;
 
+/**
+ * @brief HD44780U start of line address
+ * 
+ * @details Each character space on the display corresponds to a DDRAM address. The 
+ *          addresses count up from 0x00 to 0x67 which corresponds to 80 characters in 
+ *          a 20x4 dsiplay. The addresses start at the beginning of line 1 (0x00) and 
+ *          carry over to line 3, then line 2, and lastly line 4 which is the reason 
+ *          for the order in the enum below. These addresses can be used to format 
+ *          the information that gets sent to the screen. 
+ * 
+ */
+typedef enum {
+    HD44780U_START_L1 = 0x80,
+    HD44780U_START_L3 = 0x94,
+    HD44780U_START_L2 = 0xC0,
+    HD44780U_START_L4 = 0xD4
+} hd44780u_line_start_position_t;
+
 //=======================================================================================
 
 
@@ -118,7 +165,7 @@ typedef enum {
 // Function Prototypes
 
 /**
- * @brief LCD screen init 
+ * @brief LCD screen initialization sequence. 
  * 
  */
 void hd44780u_init(void);
@@ -126,26 +173,46 @@ void hd44780u_init(void);
 /**
  * @brief LCD send command
  * 
- * @details The command for the LCD screen gets sent here from hd44780u_init, formatted, 
- *          and send to the LCD using I2C. 
+ * @details Instructions for the LCD screen get sent here from hd44780u_init, 
+ *          formatted, and forwarded to the LCD using I2C. This function is also used
+ *          for setting the DDRAM value to choose a position on the screen. 
  * 
- * @param hd44780u_cmd 
+ * @param hd44780u_cmd : instruction to configure the screen 
  */
-void hd44780u_init_send(uint8_t hd44780u_cmd);
+void hd44780u_send_instruc(uint8_t hd44780u_cmd);
 
 /**
  * @brief LCD send data
  * 
- * @param hd44780u_data 
+ * @details All the information that gets printed on the screen is sent using this 
+ *          function. It sends a single byte of information on each pass. Command data 
+ *          gets formatted before sending to the screen. 
+ * 
+ * @param hd44780u_data : command to be printed to the screen
  */
 void hd44780u_send_data(uint8_t hd44780u_data);
 
 /**
  * @brief LCD send string
  * 
- * @param print_string 
+ * @details Sends a whole string of data to be printed on the screen. One character 
+ *          or byte is sent at a time using the hd44780u_send_data function. 
+ * 
+ * @see hd44780u_send_data
+ * 
+ * @param print_string : string of data that gets printed to the screen
  */
 void hd44780u_send_string(char *print_string);
+
+/**
+ * @brief Clear LCD display 
+ * 
+ * @details Blank characters are send to all character spaces on the screen to erase 
+ *          any existing text. The DDRAM address also gets updates to the cursor sits 
+ *          at the beginning of line 1. 
+ * 
+ */
+void hd44780u_clear(void);
 
 //=======================================================================================
 
--- sources/wayintop_lcd_driver.c
+++ sources/wayintop_lcd_driver.c
@@ -3,7 +3,7 @@
  * 
  * @author Sam Donnelly (samueldonnelly11@gmail.com)
  * 
- * @brief 
+ * @brief Wayintop 2004 LCD screen (HD44780U + PCF8574) driver
  * 
  * @version 0.1
  * @date 2022-02-11
@@ -44,57 +44,60 @@ void hd44780u_init(void)
     tim9_delay_ms(HD44780U_DELAY_050MS);
 
     // 2. Function set. Wait for more than 4.1 ms. 
-    hd44780u_init_send(HD44780U_SETUP_CMD_0X30);
+    hd44780u_send_instruc(HD44780U_SETUP_CMD_0X30);
     tim9_delay_ms(HD44780U_DELAY_005MS);
 
     // 3. Function set. Wait for more than 100 us. 
-    hd44780u_init_send(HD44780U_SETUP_CMD_0X30);
+    hd44780u_send_instruc(HD44780U_SETUP_CMD_0X30);
     tim9_delay_us(HD44780U_DELAY_200US);
 
     // 4. Function set. No specified wait time. 
-    hd44780u_init_send(HD44780U_SETUP_CMD_0X30);
+    hd44780u_send_instruc(HD44780U_SETUP_CMD_0X30);
     tim9_delay_ms(HD44780U_DELAY_010MS);
 
     // 5. Function set - Choose 4-bit mode
     // DL = 0 -> 4-bit data length 
-    hd44780u_init_send(HD44780U_SETUP_CMD_0X20);
+    hd44780u_send_instruc(HD44780U_SETUP_CMD_0X20);
     tim9_delay_ms(HD44780U_DELAY_001MS);
 
     // 6. Function set - Specify the number of display lines and character font
     // N = 1  -> Sets the number of dsiplay lines to 2 
     // F = 0  -> Sets character font to 5x8 dots
-    hd44780u_init_send(HD44780U_SETUP_CMD_0X28);
+    hd44780u_send_instruc(HD44780U_SETUP_CMD_0X28);
     tim9_delay_ms(HD44780U_DELAY_001MS);
 
     // 7. Display off 
     // D = 0 -> Display off 
     // C = 0 -> Cursor not displayed 
     // B = 0 -> No blinking 
-    hd44780u_init_send(HD44780U_SETUP_CMD_0x08);
+    hd44780u_send_instruc(HD44780U_SETUP_CMD_0x08);
     tim9_delay_ms(HD44780U_DELAY_001MS);
 
     // 8. Display clear 
-    hd44780u_init_send(HD44780U_SETUP_CMD_0X01);
+    hd44780u_send_instruc(HD44780U_SETUP_CMD_0X01);
     tim9_delay_ms(HD44780U_DELAY_001MS);
 
     // 9. Entry mode set 
     // I/D = 1 -> Increment 
     // S = 0   -> No display shifting 
-    hd44780u_init_send(HD44780U_SETUP_CMD_0X06);
+    hd44780u_send_instruc(HD44780U_SETUP_CMD_0X06);
     tim9_delay_ms(HD44780U_DELAY_001MS);
 
     // 10. Display on 
     // D = 1 -> Display on
     // C = 0 -> Cursor not displayed 
     // B = 0 -> No blinking 
-    hd44780u_init_send(HD44780U_SETUP_CMD_0X0C);
+    hd44780u_send_instruc(HD44780U_SETUP_CMD_0X0C);
     tim9_delay_ms(HD44780U_DELAY_001MS);
+
+    // Clear the display 
+    hd44780u_clear();
 }
 
-// HD44780U init command send function
-void hd44780u_init_send(uint8_t hd44780u_cmd)
+// Send single byte of instruction information 
+void hd44780u_send_instruc(uint8_t hd44780u_cmd)
 {
-    // Data list 
+    // Organize send data into a sendable format
     uint8_t lcd_setup_data[HD44780U_MSG_PER_CMD];
     lcd_setup_data[0] = (hd44780u_cmd & 0xF0) | HD44780U_CONFIG_CMD_0X0C;
     lcd_setup_data[1] = (hd44780u_cmd & 0xF0) | HD44780U_CONFIG_CMD_0X08;
@@ -114,10 +117,10 @@ void hd44780u_init_send(uint8_t hd44780u_cmd)
 //=======================================================================================
 // Print data 
 
-// HD44780U send data 
+// Send single byte of printable data 
 void hd44780u_send_data(uint8_t hd44780u_data)
 {
-    // Data list 
+    // Organize send data into a sendable format
     uint8_t lcd_print_data[HD44780U_MSG_PER_CMD];
     lcd_print_data[0] = (hd44780u_data & 0xF0) | HD44780U_CONFIG_CMD_0X0D;
     lcd_print_data[1] = (hd44780u_data & 0xF0) | HD44780U_CONFIG_CMD_0X09;
@@ -131,14 +134,28 @@ void hd44780u_send_data(uint8_t hd44780u_data)
         PCF8574_HHH_WRITE_ADDRESS);
 }
 
-// HD44780U send string
+// Send a string of data string
 void hd44780u_send_string(char *print_string)
 {
-    // Send one character at a time
+    // Send one string character at a time
     while(*print_string)
     {
+        // Cast from char to integer for send_data function 
         hd44780u_send_data((uint8_t)(*print_string));
         print_string++;
+    }
+}
+
+// Clear the HD44780U screen 
+void hd44780u_clear(void)
+{
+    // Set the write address to the first place on the screen 
+    hd44780u_send_instruc(HD44780U_START_L1);
+
+    // Send a blank string to each display character to clear the display
+    for(uint8_t i = 0; i < HD44780U_NUM_CHAR; i++)
+    {
+        hd44780u_send_data(' ');
     }
 }
 
