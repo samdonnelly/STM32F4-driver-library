6774a27678d14639887249b4b61234756adcc8b2
Sam Donnelly <samueldonnelly11@gmail.com>
2022-10-23 15:10:26 -0600

    Started setting up the analog driver.

--- sources/analog_driver.c
+++ sources/analog_driver.c
@@ -21,3 +21,31 @@
 // Libraries 
 
 //=======================================================================================
+
+
+//================================================================================
+// Initialization 
+
+// ADC port init 
+void adc_port_init(void)
+{
+    // Enable the ADC1 clock 
+    RCC->APB2ENR |= (SET_BIT << SHIFT_8); 
+}
+
+
+// ADC init 
+void adc_pin_init(void)
+{
+    // Notes: 
+    //  - This function gets called for each ADC channel being initialized so don't put 
+    //    code that only needs to be called once in here. 
+
+    // Configure the clock 
+
+    // Configure the GPIOs for analog mode 
+
+    // Configure the ADC 
+}
+
+//================================================================================
