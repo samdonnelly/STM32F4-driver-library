09ed29a036a9ecd5a0b02b4714c6d31eca39408e
Sam Donnelly <samueldonnelly11@gmail.com>
2022-10-23 21:34:24 -0600

    Added more initialization code to the analog driver.

--- headers/analog_driver.h
+++ headers/analog_driver.h
@@ -30,6 +30,81 @@
 
 
 //================================================================================
+// Enums 
+
+/**
+ * @brief 
+ * 
+ * @details 
+ * 
+ */
+typedef enum {
+    ADC_CHANNEL_0,    // 
+    ADC_CHANNEL_1,    // 
+    ADC_CHANNEL_2,    // 
+    ADC_CHANNEL_3,    // 
+    ADC_CHANNEL_4,    // 
+    ADC_CHANNEL_5,    // 
+    ADC_CHANNEL_6,    // 
+    ADC_CHANNEL_7,    // 
+    ADC_CHANNEL_8,    // 
+    ADC_CHANNEL_9,    // 
+    ADC_CHANNEL_10,    // 
+    ADC_CHANNEL_11,    // 
+    ADC_CHANNEL_12,    // 
+    ADC_CHANNEL_13,    // 
+    ADC_CHANNEL_14,    // 
+    ADC_CHANNEL_15     // 
+} adc_channel_t; 
+
+
+/**
+ * @brief 
+ * 
+ * @details 
+ * 
+ */
+typedef enum {
+    ADC_SMP_3,     // 3 cycles 
+    ADC_SMP_15,    // 15 cycles
+    ADC_SMP_28,    // 28 cycles
+    ADC_SMP_56,    // 56 cycles
+    ADC_SMP_84,    // 84 cycles
+    ADC_SMP_112,   // 112 cycles
+    ADC_SMP_144,   // 144 cycles
+    ADC_SMP_480    // 480 cycles
+} adc_smp_cycles_t; 
+
+
+/**
+ * @brief 
+ * 
+ * @details 
+ * 
+ */
+typedef enum {
+    ADC_SEQ_1 = 1,    // 
+    ADC_SEQ_2,        // 
+    ADC_SEQ_3,        // 
+    ADC_SEQ_4,        // 
+    ADC_SEQ_5,        // 
+    ADC_SEQ_6,        // 
+    ADC_SEQ_7,        // 
+    ADC_SEQ_8,        // 
+    ADC_SEQ_9,        // 
+    ADC_SEQ_10,       // 
+    ADC_SEQ_11,       // 
+    ADC_SEQ_12,       // 
+    ADC_SEQ_13,       // 
+    ADC_SEQ_14,       // 
+    ADC_SEQ_15,       // 
+    ADC_SEQ_16        // 
+} adc_seq_num_t; 
+
+//================================================================================
+
+
+//================================================================================
 // Function prototypes 
 
 /**
@@ -44,8 +119,50 @@ void adc_port_init(void);
  * 
  * @details 
  * 
+ * @param adc 
+ * @param gpio 
+ * @param adc_pin 
+ * @param adc_channel 
+ * @param adc_smp 
+ * @param adc_seq_num : conversion number of channel in the regular sequence 
  */
-void adc_pin_init(void); 
+void adc_pin_init(
+    ADC_TypeDef *adc, 
+    GPIO_TypeDef *gpio, 
+    pin_selector_t adc_pin, 
+    adc_channel_t adc_channel, 
+    adc_smp_cycles_t smp, 
+    adc_seq_num_t seq_num); 
+
+
+/**
+ * @brief Regular channel sequence length setter 
+ * 
+ * @details 
+ * 
+ * @param adc : 
+ * @param seq_len : number of conversions in the regular channel conversion sequence 
+ */
+void adc_seq_len_set(
+    ADC_TypeDef *adc, 
+    adc_seq_num_t seq_len); 
+
+//================================================================================
+
+
+//================================================================================
+// Read 
+
+/**
+ * @brief ADC data register read 
+ * 
+ * @details 
+ * 
+ * @param adc 
+ * @return uint16_t 
+ */
+uint16_t adc_dr(
+    ADC_TypeDef *adc); 
 
 //================================================================================
 
--- sources/analog_driver.c
+++ sources/analog_driver.c
@@ -35,17 +35,59 @@ void adc_port_init(void)
 
 
 // ADC init 
-void adc_pin_init(void)
+void adc_pin_init(
+    ADC_TypeDef *adc, 
+    GPIO_TypeDef *gpio, 
+    pin_selector_t adc_pin, 
+    adc_channel_t adc_channel, 
+    adc_smp_cycles_t smp, 
+    adc_seq_num_t seq_num)
 {
     // Notes: 
     //  - This function gets called for each ADC channel being initialized so don't put 
     //    code that only needs to be called once in here. 
 
-    // Configure the clock 
-
-    // Configure the GPIOs for analog mode 
+    // Configure the GPIO for analog mode 
+    gpio->MODER |= (SET_3 << (2*adc_pin)); 
+    // gpio->OSPEEDR |= (SET_3 << (2*adc_pin)); 
 
     // Configure the ADC 
+
+    // Set the sample time for the channel 
+    if (adc_channel > ADC_CHANNEL_9) 
+        adc->SMPR1 |= (smp << 3*(adc_channel - ADC_CHANNEL_10)); 
+    else 
+        adc->SMPR2 |= (smp << 3*adc_channel); 
+
+    // Set the channel sequence number 
+    if (seq_num > ADC_SEQ_12) 
+        adc->SQR1 |= (adc_channel << 5*(seq_num - ADC_SEQ_13)); 
+    else if (seq_num > ADC_SEQ_6) 
+        adc->SQR2 |= (adc_channel << 5*(seq_num - ADC_SEQ_7)); 
+    else 
+        adc->SQR3 |= (adc_channel << 5*(seq_num - ADC_SEQ_1)); 
+}
+
+
+// Regular channel sequence length setter 
+void adc_seq_len_set(
+    ADC_TypeDef *adc, 
+    adc_seq_num_t seq_len) 
+{
+    adc->SQR1 |= (seq_len << SHIFT_20); 
+}
+
+//================================================================================
+
+
+//================================================================================
+// Read 
+
+// ADC data register read 
+uint16_t adc_dr(
+    ADC_TypeDef *adc)
+{
+    return (uint16_t)(adc->DR); 
 }
 
 //================================================================================
