6deef209c69d3a36df5cd544abe01713943e5fcd
Sam Donnelly <samueldonnelly11@gmail.com>
2022-11-04 21:59:33 -0600

    Added register functions to the interrupt driver.

--- headers/interrupt_driver.h
+++ headers/interrupt_driver.h
@@ -31,6 +31,29 @@
 
 //================================================================================
 // Macros 
+
+// EXTI Lines 
+#define EXTI_LINE_1   0x00000001 
+#define EXTI_LINE_2   0x00000002 
+#define EXTI_LINE_3   0x00000004 
+#define EXTI_LINE_4   0x00000008 
+#define EXTI_LINE_5   0x00000010 
+#define EXTI_LINE_6   0x00000020 
+#define EXTI_LINE_7   0x00000040 
+#define EXTI_LINE_8   0x00000080 
+#define EXTI_LINE_9   0x00000100 
+#define EXTI_LINE_10  0x00000200 
+#define EXTI_LINE_11  0x00000400 
+#define EXTI_LINE_12  0x00000800 
+#define EXTI_LINE_13  0x00001000 
+#define EXTI_LINE_14  0x00002000 
+#define EXTI_LINE_15  0x00004000 
+#define EXTI_LINE_16  0x00008000 
+#define EXTI_LINE_17  0x00010000 
+#define EXTI_LINE_18  0x00020000 
+#define EXTI_LINE_21  0x00100000 
+#define EXTI_LINE_22  0x00200000 
+
 //================================================================================
 
 
@@ -40,7 +63,163 @@
 
 
 //================================================================================
-// Functions 
+// Register Functions 
+
+/**
+ * @brief Interrupt mask register set 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param im 
+ */
+void exti_imr_set(
+    EXTI_TypeDef *exti, 
+    uint32_t im); 
+
+
+/**
+ * @brief Interrupt mask register clear 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param im 
+ */
+void exti_imr_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t im); 
+
+
+/**
+ * @brief Event mask register set 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param em 
+ */
+void exti_emr_set(
+    EXTI_TypeDef *exti, 
+    uint32_t em); 
+
+
+/**
+ * @brief Event mask register clear 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param em 
+ */
+void exti_emr_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t em); 
+
+
+/**
+ * @brief Rising trigger selection register set 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param rt 
+ */
+void exti_rtsr_set(
+    EXTI_TypeDef *exti, 
+    uint32_t rt); 
+
+
+/**
+ * @brief Rising trigger selection register clear 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param rt 
+ */
+void exti_rtsr_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t rt); 
+
+
+/**
+ * @brief Falling trigger selection register set 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param ft 
+ */
+void exti_ftsr_set(
+    EXTI_TypeDef *exti, 
+    uint32_t ft); 
+
+
+/**
+ * @brief Falling trigger selection register clear 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param ft 
+ */
+void exti_ftsr_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t ft); 
+
+
+/**
+ * @brief Software interrupt event register set 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param swier 
+ */
+void exti_swier_set(
+    EXTI_TypeDef *exti, 
+    uint32_t swier); 
+
+
+/**
+ * @brief Software interrupt event register clear 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param swier 
+ */
+void exti_swier_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t swier); 
+
+
+/**
+ * @brief Pending register set 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param pr 
+ */
+void exti_pr_set(
+    EXTI_TypeDef *exti, 
+    uint32_t pr); 
+
+
+/**
+ * @brief Pending register clear 
+ * 
+ * @details 
+ * 
+ * @param exti 
+ * @param pr 
+ */
+void exti_pr_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t pr); 
+
 //================================================================================
 
 #endif   // _INTERRUPT_DRIVER_H_
--- sources/interrupt_driver.c
+++ sources/interrupt_driver.c
@@ -21,5 +21,113 @@
 
 
 //================================================================================
-// Functions 
+// Register Functions 
+
+// Interrupt mask register set 
+void exti_imr_set(
+    EXTI_TypeDef *exti, 
+    uint32_t im)
+{
+    exti->IMR |= im; 
+}
+
+
+// Interrupt mask register clear 
+void exti_imr_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t im)
+{
+    exti->IMR &= ~im; 
+}
+
+
+// Event mask register set 
+void exti_emr_set(
+    EXTI_TypeDef *exti, 
+    uint32_t em)
+{
+    exti->EMR |= em; 
+}
+
+
+// Event mask register clear 
+void exti_emr_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t em)
+{
+    exti->EMR &= ~em; 
+}
+
+
+// Rising trigger selection register set 
+void exti_rtsr_set(
+    EXTI_TypeDef *exti, 
+    uint32_t rt)
+{
+    exti->RTSR |= rt; 
+}
+
+
+// Rising trigger selection register clear 
+void exti_rtsr_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t rt)
+{
+    exti->RTSR &= ~rt; 
+}
+
+
+// Falling trigger selection register set 
+void exti_ftsr_set(
+    EXTI_TypeDef *exti, 
+    uint32_t ft)
+{
+    exti->FTSR |= ft; 
+}
+
+
+// Falling trigger selection register clear 
+void exti_ftsr_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t ft)
+{
+    exti->FTSR &= ~ft; 
+}
+
+
+// Software interrupt event register set 
+void exti_swier_set(
+    EXTI_TypeDef *exti, 
+    uint32_t swier)
+{
+    exti->SWIER |= swier; 
+}
+
+
+// Software interrupt event register clear 
+void exti_swier_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t swier)
+{
+    exti->SWIER &= ~swier; 
+}
+
+
+// Pending register set 
+void exti_pr_set(
+    EXTI_TypeDef *exti, 
+    uint32_t pr)
+{
+    exti->PR |= pr; 
+}
+
+
+// Pending register clear 
+void exti_pr_clear(
+    EXTI_TypeDef *exti, 
+    uint32_t pr)
+{
+    exti->PR &= ~pr; 
+}
+
 //================================================================================
