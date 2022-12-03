97588266f84ae09ddf4a19177420d5dd128db55d
Sam <samueldonnelly11@gmail.com>
2022-12-02 20:09:05 -0700

    Added the LCD screen state machine to the controller. Still needs to be tested.

--- headers/hd44780u_controller.h
+++ headers/hd44780u_controller.h
@@ -31,13 +31,27 @@
 //================================================================================
 // Macros 
 
-#define HD44780U_NUM_STATES 1    // Number of states for the device 
+#define HD44780U_NUM_STATES 7    // Number of states for the device 
 
 //================================================================================
 
 
 //================================================================================
 // Enums 
+
+/**
+ * @brief HD44780U controller states 
+ */
+typedef enum {
+    HD44780U_INIT_STATE, 
+    HD44780U_IDLE_STATE, 
+    HD44780U_WRITE_STATE, 
+    HD44780U_READ_STATE, 
+    HD44780U_LOW_PWR_STATE, 
+    HD44780U_FAULT_STATE, 
+    HD44780U_RESET_STATE 
+} hd44780u_states_t; 
+
 //================================================================================
 
 
@@ -45,10 +59,21 @@
 // Structures 
 
 /**
- * @brief 
+ * @brief HD44780U controller trackers 
  */
-typedef struct hd44780u_trackers_s {
-    // 
+typedef struct hd44780u_trackers_s 
+{
+    // Device and controller information 
+    hd44780u_states_t state;                // State of the controller 
+    uint8_t fault_code;                     // Fault code of the device/controller 
+    uint8_t read_msg[20];                   // Buffer to store read messages 
+
+    // State flags 
+    uint8_t write     : 1;                  // Write state trigger 
+    uint8_t read      : 1;                  // Read state trigger 
+    uint8_t low_power : 1;                  // Low power state trigger 
+    uint8_t reset     : 1;                  // Reset state trigger 
+    uint8_t startup   : 1;                  // Ensures the init state is run 
 }
 hd44780u_trackers_t; 
 
@@ -71,6 +96,14 @@ typedef void (*hd44780u_state_functions_t)(
 
 //================================================================================
 // Function prototypes 
+
+
+void hd44780u_controller_init(void); 
+
+
+
+void hd44780u_controller(void); 
+
 //================================================================================
 
 #endif   // _HD44780U_CONTROLLER_H_ 
--- sources/hd44780u_controller.c
+++ sources/hd44780u_controller.c
@@ -38,6 +38,72 @@
 void hd44780u_init_state(
     hd44780u_trackers_t hd44780u_device); 
 
+
+/**
+ * @brief 
+ * 
+ * @details 
+ * 
+ * @param hd44780u_device 
+ */
+void hd44780u_idle_state(
+    hd44780u_trackers_t hd44780u_device); 
+
+
+/**
+ * @brief 
+ * 
+ * @details 
+ * 
+ * @param hd44780u_device 
+ */
+void hd44780u_write_state(
+    hd44780u_trackers_t hd44780u_device); 
+
+
+/**
+ * @brief 
+ * 
+ * @details 
+ * 
+ * @param hd44780u_device 
+ */
+void hd44780u_read_state(
+    hd44780u_trackers_t hd44780u_device); 
+
+
+/**
+ * @brief 
+ * 
+ * @details 
+ * 
+ * @param hd44780u_device 
+ */
+void hd44780u_low_power_state(
+    hd44780u_trackers_t hd44780u_device); 
+
+
+/**
+ * @brief 
+ * 
+ * @details 
+ * 
+ * @param hd44780u_device 
+ */
+void hd44780u_fault_state(
+    hd44780u_trackers_t hd44780u_device); 
+
+
+/**
+ * @brief 
+ * 
+ * @details 
+ * 
+ * @param hd44780u_device 
+ */
+void hd44780u_reset_state(
+    hd44780u_trackers_t hd44780u_device); 
+
 //================================================================================
 
 
@@ -45,32 +111,206 @@ void hd44780u_init_state(
 // Variables 
 
 // Instance of the device tracker record 
-hd44780u_trackers_t hd44780u_device; 
+static hd44780u_trackers_t hd44780u_device_trackers; 
 
 // Function pointers 
 static hd44780u_state_functions_t state_table[HD44780U_NUM_STATES] = 
 {
-    &hd44780u_init_state 
+    &hd44780u_init_state, 
+    &hd44780u_idle_state, 
+    &hd44780u_write_state, 
+    &hd44780u_read_state, 
+    &hd44780u_low_power_state, 
+    &hd44780u_fault_state, 
+    &hd44780u_reset_state 
 }; 
 
 //================================================================================
 
 
 //================================================================================
-// Functions 
+// Control Functions 
 
 // Initialization 
+void hd44780u_controller_init(void)
+{
+    hd44780u_device_trackers.state = HD44780U_INIT_STATE; 
+
+    hd44780u_device_trackers.fault_code = CLEAR; 
+
+    hd44780u_device_trackers.write = CLEAR_BIT; 
+
+    hd44780u_device_trackers.read = CLEAR_BIT; 
+
+    hd44780u_device_trackers.low_power = CLEAR_BIT; 
+
+    hd44780u_device_trackers.reset = CLEAR_BIT; 
+
+    hd44780u_device_trackers.startup = SET_BIT; 
+}
 
 
 // Controller 
+void hd44780u_controller(void)
+{
+    // Local variables 
+    hd44780u_states_t next_state = hd44780u_device_trackers.state; 
+
+    //==================================================
+    // State machine 
+
+    // Fault state 
+    if (hd44780u_device_trackers.fault_code)
+    {
+        // Check the current state status 
+        if (next_state == HD44780U_FAULT_STATE)
+        {
+            if (hd44780u_device_trackers.reset)
+                next_state = HD44780U_RESET_STATE; 
+        }
+        else 
+            next_state = HD44780U_FAULT_STATE; 
+    }
+
+    // Reset state 
+    else if (hd44780u_device_trackers.reset)
+    {
+        if (next_state == HD44780U_RESET_STATE)
+            next_state = HD44780U_INIT_STATE; 
+        else 
+            next_state = HD44780U_RESET_STATE; 
+    }
+
+    // Other states 
+    else 
+    {
+        switch (next_state)
+        {
+            case HD44780U_INIT_STATE: 
+                if (!(hd44780u_device_trackers.startup))
+                    next_state = HD44780U_IDLE_STATE; 
+                break; 
+
+            case HD44780U_IDLE_STATE: 
+                if (hd44780u_device_trackers.write)
+                    next_state = HD44780U_WRITE_STATE; 
+                
+                else if (hd44780u_device_trackers.read)
+                    next_state = HD44780U_READ_STATE; 
+
+                else if (hd44780u_device_trackers.low_power)
+                    next_state = HD44780U_LOW_PWR_STATE; 
+                
+                break; 
+
+            case HD44780U_WRITE_STATE: 
+                if (hd44780u_device_trackers.read)
+                    next_state = HD44780U_READ_STATE; 
+
+                else if (!(hd44780u_device_trackers.write))
+                    next_state = HD44780U_IDLE_STATE; 
+                
+                break; 
+
+            case HD44780U_READ_STATE: 
+                if (hd44780u_device_trackers.write)
+                    next_state = HD44780U_WRITE_STATE; 
+
+                else if (!(hd44780u_device_trackers.read))
+                    next_state = HD44780U_IDLE_STATE; 
+                
+                break; 
+
+            case HD44780U_LOW_PWR_STATE: 
+                if (!(hd44780u_device_trackers.low_power))
+                    next_state = HD44780U_IDLE_STATE; 
+                break; 
+
+            default: 
+                break; 
+        }
+    }
+
+    //==================================================
+
+    // Go to state function 
+    (state_table[next_state])(hd44780u_device_trackers); 
+
+    // Update the state 
+    hd44780u_device_trackers.state = next_state; 
+}
+
+//================================================================================
+// State functions 
+
+// 
+void hd44780u_init_state(
+    hd44780u_trackers_t hd44780u_device)
+{
+    // Clear reset flag 
+    // Clear startup flag 
+}
 
 
-// States 
+// 
+void hd44780u_idle_state(
+    hd44780u_trackers_t hd44780u_device)
+{
+    // Do basically nothing 
+}
 
 
+// 
+void hd44780u_write_state(
+    hd44780u_trackers_t hd44780u_device)
+{
+    // Clear the write flag 
+}
+
+
+// 
+void hd44780u_read_state(
+    hd44780u_trackers_t hd44780u_device)
+{
+    // Clear the read flag 
+}
+
+
+// 
+void hd44780u_low_power_state(
+    hd44780u_trackers_t hd44780u_device)
+{
+    // Put the screen in low power mode. 
+    // Should this just be a transition state? You'd call this then return to idle 
+    // then go to a different state to bring the screen out of low power. 
+}
+
+
+// 
+void hd44780u_fault_state(
+    hd44780u_trackers_t hd44780u_device)
+{
+    // 
+}
+
+
+// 
+void hd44780u_reset_state(
+    hd44780u_trackers_t hd44780u_device)
+{
+    // Clear the fault code 
+}
+
+//================================================================================
+
+
+//================================================================================
 // Setters 
+//================================================================================
 
 
+//================================================================================
 // Getters 
+//================================================================================
 
 //================================================================================
