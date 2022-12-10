5cefb67fd35cccfcd02312dfe9b56bb3d917fa8f
Sam Donnelly <samueldonnelly11@gmail.com>
2022-12-09 22:29:20 -0700

    LCD screen controller comments.

--- sources/hd44780u_controller.c
+++ sources/hd44780u_controller.c
@@ -42,10 +42,10 @@ void hd44780u_init_state(
 /**
  * @brief HD44780U idle state 
  * 
- * @details Resting state of the device during normal operation. When the screen is not performing 
- *          a task then the controller defaults to the idle state where no action is taken until a 
- *          flag is set that changes the state. Having this state allows for the code to get in and 
- *          out as quickly as possible when no work needs to be doen. 
+ * @details Resting state of the device during normal operation. When the screen is not 
+ *          performing a task then the controller defaults to the idle state where no action 
+ *          is taken until a flag is set that changes the state. Having this state allows for 
+ *          the code to get in and out as quickly as possible when no work needs to be done. 
  * 
  * @param hd44780u_device : device tracker that defines control characteristics 
  */
@@ -56,13 +56,14 @@ void hd44780u_idle_state(
 /**
  * @brief HD44780U write state 
  * 
- * @details Writes the contents of the devices data record to the screen. To trigger this state, the 
- *          write flag should be set via the setter function. At the end of this state, the write flag 
- *          is automatically cleared the state machine returns to idle if no other flags are set. <br> 
+ * @details Writes the contents of the devices data record to the screen. To trigger this state, 
+ *          the write flag should be set via the setter function. At the end of this state, the 
+ *          write flag is automatically cleared the state machine returns to idle if no other 
+ *          flags are set. <br> 
  *          
- *          The contents of the data record can be updated through the use of any of the line set or 
- *          line clear functions. The results of updating the data record won't be visible on the 
- *          screen until the write state occurs. 
+ *          The contents of the data record can be updated through the use of any of the line 
+ *          set or line clear functions. The results of updating the data record won't be 
+ *          visible on the screen until the write state occurs. 
  * 
  * @see hd44780_set_write_flag
  * @see hd44780u_line1_set
@@ -83,18 +84,19 @@ void hd44780u_write_state(
 /**
  * @brief HD44780U read state 
  * 
- * @details Currently this state does nothing, it is only a placeholder for a working read state. This 
- *          state is called when the read flag is set via the setter function. Once this state is called 
- *          the read flag is instantly cleared and the controller returns to the idle state if no 
- *          other flags are set. <br> 
+ * @details Currently this state does nothing, it is only a placeholder for a working read 
+ *          state. This state is called when the read flag is set via the setter function. 
+ *          Once this state is called the read flag is instantly cleared and the controller 
+ *          returns to the idle state if no other flags are set. <br> 
  *          
- *          Ideally this state could read status information from the screen such as fault codes. 
- *          However, reading from the screen is not yet supported and it is unknown what information 
- *          can be read. Once read functionaility is implemented, the read flag setter function should 
- *          be changed to take a message specifier as an argument. <br> 
+ *          Ideally this state could read status information from the screen such as fault 
+ *          codes. However, reading from the screen is not yet supported and it is unknown 
+ *          what information can be read. Once read functionaility is implemented, the read 
+ *          flag setter function should be changed to take a message specifier as an argument. 
+ *          <br> 
  *          
- *          To get the contents of a device read (once functionality is added), the read message getter 
- *          can be called to obtain the return message. 
+ *          To get the contents of a device read (once functionality is added), the read 
+ *          message getter can be called to obtain the return message. 
  * 
  * @see hd44780_set_read_flag
  * @see hd44780u_get_read_msg
@@ -108,11 +110,12 @@ void hd44780u_read_state(
 /**
  * @brief HD44780U low power mode transition state 
  * 
- * @details Allows for transitioning into the controllers low power mode state. When the low power 
- *          mode flag is set, the controller will call this state which in turn shuts the screen 
- *          off and then proceeds immediately into the low power mode state. When the low power 
- *          mode flag is cleared, the controller will move from the low power mode state into 
- *          this state where the screen gets turned back on before returning to idle. 
+ * @details Allows for transitioning into the controllers low power mode state. When the low 
+ *          power mode flag is set, the controller will call this state which in turn shuts 
+ *          the screen off and then proceeds immediately into the low power mode state. When 
+ *          the low power mode flag is cleared, the controller will move from the low power 
+ *          mode state into this state where the screen gets turned back on before returning 
+ *          to idle. 
  * 
  * @see hd44780u_set_low_pwr_flag
  * @see hd44780u_clear_low_pwr_flag
@@ -126,7 +129,16 @@ void hd44780u_low_pwr_trans_state(
 /**
  * @brief HD44780U low power mode state 
  * 
- * @details 
+ * @details This state is entered when the low power mode flag is set. The controller first 
+ *          passes through the low power mode transition state where the screen is turned off. 
+ *          After the transition state the controller enters this state where it idles until 
+ *          the low power flag is cleared. In this state the sceen cannot be written to or 
+ *          read from. This state is used to consume less power at times the screen is not 
+ *          needed. 
+ * 
+ * @see hd44780u_low_pwr_trans_state
+ * @see hd44780u_set_low_pwr_flag
+ * @see hd44780u_clear_low_pwr_flag
  * 
  * @param hd44780u_device : device tracker that defines control characteristics 
  */
@@ -137,7 +149,13 @@ void hd44780u_low_pwr_state(
 /**
  * @brief HD44780U fault state 
  * 
- * @details 
+ * @details Currently this state is not used. To enter this state the fault code must be set 
+ *          to non-zero and there are currently no methods in place in the controller to set 
+ *          the fault code. Regardless of value, the fault code gets cleared in the reset 
+ *          state. <br> 
+ *          
+ *          This state takes the highest priority meaning the controller will enter this state 
+ *          regardless of any other flags set. 
  * 
  * @param hd44780u_device : device tracker that defines control characteristics 
  */
