3208da9320a55be3e67d4baed0ec13446af6947e
Sam <samueldonnelly11@gmail.com>
2023-01-15 21:55:10 -0700

    Started filling out hw125 controller.

--- headers/hw125_controller.h
+++ headers/hw125_controller.h
@@ -22,16 +22,66 @@
 
 //=======================================================================================
 // Macros 
+
+#define HW125_NUM_STATES 7           // Number of possible states for the controller 
+
+#define HW125_BUFF_SIZE 100          // Read and write buffer size 
+#define HW125_INFO_SIZE 30           // Device infor buffer size 
+
 //=======================================================================================
 
 
 //=======================================================================================
 // Enums 
+
+/**
+ * @brief HW125 controller states 
+ */
+typedef enum {
+    HW125_INIT_STATE, 
+    HW125_STANDBY_STATE, 
+    HW125_OPEN_STATE, 
+    HW125_CLOSE_STATE, 
+    HW125_ACCESS_STATE, 
+    HW125_FAULT_STATE, 
+    HW125_RESET_STATE 
+} hw125_states_t; 
+
 //=======================================================================================
 
 
 //=======================================================================================
 // Structures 
+
+// HW125 controller trackers 
+typedef struct hw125_trackers_s 
+{
+    // Controller information 
+    hw125_states_t state;                        // State of the controller 
+    uint8_t fault_code;                          // Fault code of the device/controller 
+
+    // File system information 
+    TCHAR path;                                  // Path to desired directory 
+    FATFS *file_sys;                             // 
+    FIL file;                                    // 
+    FRESULT result;                              // 
+    UINT br;                                     // Read counter 
+    UINT bw;                                     // Write counter 
+    char data_buff[HW125_BUFF_SIZE];             // Buffer to store read and write data 
+    FATFS *pfs;                                  // 
+    DWORD fre_clust;                             // 
+    uint32_t total;                              // Volume total space 
+    uint32_t free_space;                         // Volume free space 
+    TCHAR vol_label[HW125_INFO_SIZE];            // Volume label 
+    DWORD serial_num;                            // Volume serial number 
+
+    // State trackers 
+    uint8_t open_file : 1;                       // Open file flag 
+    uint8_t reset     : 1;                       // Reset state trigger 
+    uint8_t startup   : 1;                       // Ensures the init state is run 
+}
+hw125_trackers_t; 
+
 //=======================================================================================
 
 
@@ -42,6 +92,16 @@
 
 //=======================================================================================
 // Function pointers 
+
+/**
+ * @brief HW125 state machine function pointer 
+ * 
+ * @param hw125_device : device tracker that defines control characteristics 
+ * 
+ */
+typedef void (*hw125_state_functions_t)(
+    hw125_trackers_t *hw125_device); 
+
 //=======================================================================================
 
 
--- sources/hw125_controller.c
+++ sources/hw125_controller.c
@@ -22,11 +22,104 @@
 
 //=======================================================================================
 // Function prototypes 
+
+/**
+ * @brief HW125 initialization state 
+ * 
+ * @details 
+ * 
+ * @param hw125_device : device tracker that defines control characteristics 
+ */
+void hw125_init_state(
+    hw125_trackers_t *hw125_device); 
+
+
+/**
+ * @brief HW125 idle state 
+ * 
+ * @details 
+ * 
+ * @param hw125_device : device tracker that defines control characteristics 
+ */
+void hw125_standby_state(
+    hw125_trackers_t *hw125_device); 
+
+
+/**
+ * @brief HW125 open file 
+ * 
+ * @details 
+ * 
+ * @param hw125_device : device tracker that defines control characteristics 
+ */
+void hw125_open_state(
+    hw125_trackers_t *hw125_device); 
+
+
+/**
+ * @brief HW125 close file state 
+ * 
+ * @details 
+ * 
+ * @param hw125_device : device tracker that defines control characteristics 
+ */
+void hw125_close_state(
+    hw125_trackers_t *hw125_device); 
+
+
+/**
+ * @brief HW125 access file state 
+ * 
+ * @details 
+ * 
+ * @param hw125_device : device tracker that defines control characteristics 
+ */
+void hw125_access_state(
+    hw125_trackers_t *hw125_device); 
+
+
+/**
+ * @brief HW125 fault state 
+ * 
+ * @details 
+ * 
+ * @param hw125_device : device tracker that defines control characteristics 
+ */
+void hw125_fault_state(
+    hw125_trackers_t *hw125_device); 
+
+
+/**
+ * @brief HW125 reset state 
+ * 
+ * @details 
+ * 
+ * @param hw125_device : device tracker that defines control characteristics 
+ */
+void hw125_reset_state(
+    hw125_trackers_t *hw125_device); 
+
 //=======================================================================================
 
 
 //=======================================================================================
 // Variables 
+
+// Device tracker record instance 
+static hw125_trackers_t hw125_device_trackers; 
+
+// Function pointers to controller states 
+static hw125_state_functions_t state_table[HW125_NUM_STATES] = 
+{
+    &hw125_init_state, 
+    &hw125_standby_state, 
+    &hw125_open_state, 
+    &hw125_close_state, 
+    &hw125_access_state, 
+    &hw125_fault_state, 
+    &hw125_reset_state 
+}; 
+
 //=======================================================================================
 
 
@@ -43,7 +136,46 @@ void hw125_controller_init(void)
 // HW125 controller 
 void hw125_controller(void)
 {
-    // 
+    // Local variables 
+    hw125_states_t next_state = hw125_device_trackers.state; 
+
+    //==================================================
+    // State machine 
+
+    switch (next_state)
+    {
+        case HW125_INIT_STATE: 
+            break; 
+
+        case HW125_STANDBY_STATE: 
+            break; 
+
+        case HW125_OPEN_STATE: 
+            break; 
+
+        case HW125_CLOSE_STATE: 
+            break; 
+
+        case HW125_ACCESS_STATE: 
+            break; 
+
+        case HW125_FAULT_STATE: 
+            break; 
+
+        case HW125_RESET_STATE: 
+            break; 
+
+        default: 
+            break; 
+    }
+
+    //==================================================
+
+    // Go to state function 
+    (state_table[next_state])(&hw125_device_trackers); 
+
+    // Update the state 
+    hw125_device_trackers.state = next_state; 
 }
 
 //=======================================================================================
@@ -51,11 +183,88 @@ void hw125_controller(void)
 
 //=======================================================================================
 // State functions 
+
+// HW125 initialization state 
+void hw125_init_state(
+    hw125_trackers_t *hw125_device) 
+{
+    // Clear startup bit 
+
+    // Clear reset bit 
+
+    // Mount the drive 
+
+    // Check for free space - set fault code if insuficient 
+
+    // Get and record the volume label 
+
+    // Check for the specified directory - if it does not exist then create it 
+}
+
+
+// HW125 idle state 
+void hw125_standby_state(
+    hw125_trackers_t *hw125_device) 
+{
+    // Do nothing and wait for a file to be opened 
+}
+
+
+// HW125 open file state - state may not be needed 
+void hw125_open_state(
+    hw125_trackers_t *hw125_device) 
+{
+    // 
+}
+
+
+// HW125 close file state - state may not be needed 
+void hw125_close_state(
+    hw125_trackers_t *hw125_device) 
+{
+    // 
+}
+
+
+// HW125 access file state 
+void hw125_access_state(
+    hw125_trackers_t *hw125_device) 
+{
+    // 
+}
+
+
+// HW125 fault state 
+void hw125_fault_state(
+    hw125_trackers_t *hw125_device) 
+{
+    // 
+}
+
+
+// HW125 reset state 
+void hw125_reset_state(
+    hw125_trackers_t *hw125_device) 
+{
+    // Unmount the drive 
+
+    // Clear the fault code 
+}
+
 //=======================================================================================
 
 
 //=======================================================================================
 // Setters 
+
+// hw125_mkdir 
+// - Create a new folder within the project directory 
+
+// hw125_open 
+// - Check that a file is not already open 
+// - Checks for the existance of the specified file 
+// - If the file doesn't exist and write mode is requested then create the file 
+
 //=======================================================================================
 
 
