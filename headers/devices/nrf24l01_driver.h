dcf7cba4c587495a9d4dd0cc5da831e605512c86
Sam Donnelly <samueldonnelly11@gmail.com>
2023-07-02 11:35:22 -0600

    Added library files for the nrF24L01 RF module.

---
+++ headers/devices/nrf24l01_driver.h
@@ -0,0 +1,31 @@
+/**
+ * @file nrf24l01_driver.h
+ * 
+ * @author Sam Donnelly (samueldonnelly11@gmail.com)
+ * 
+ * @brief nRF24L01 RF module driver header 
+ * 
+ * @version 0.1
+ * @date 2023-07-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef _NRF24L01_DRIVER_H_ 
+#define _NRF24L01_DRIVER_H_ 
+
+//===============================================================================
+// Includes 
+
+// Toolkit 
+#include "stm32f411xe.h"
+#include "tools.h"
+
+// Communication drivers 
+#include "i2c_comm.h"
+#include "gpio_driver.h"
+
+//===============================================================================
+
+#endif   // _NRF24L01_DRIVER_H_ 
---
+++ sources/devices/nrf24l01_driver.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file nrf24l01_driver.cpp
+ * 
+ * @author Sam Donnelly (samueldonnelly11@gmail.com)
+ * 
+ * @brief nRF24L01 RF module driver 
+ * 
+ * @version 0.1
+ * @date 2023-07-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+//===============================================================================
+// Includes 
+
+#include "nrf24l01_driver.h" 
+
+//===============================================================================
