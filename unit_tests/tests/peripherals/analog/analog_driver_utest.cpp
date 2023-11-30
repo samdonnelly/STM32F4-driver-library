2d8a38cb24b09b76f8dd1eb3c12057ebfa79ee58
Sam Donnelly <samueldonnelly11@gmail.com>
2023-11-29 22:54:33 -0700

    Analog driver unit testing.

--- unit_tests/tests/peripherals/analog/analog_driver_utest.cpp
+++ unit_tests/tests/peripherals/analog/analog_driver_utest.cpp
@@ -3,10 +3,15 @@
 extern "C"
 {
 	// Add your C-only include files here 
+    #include "analog_driver.h" 
 }
 
 //==================================================
 // Notes 
+
+// Test plan 
+// - 
+
 //==================================================
 
 TEST_GROUP(analog_driver)
@@ -28,5 +33,27 @@ TEST_GROUP(analog_driver)
 // Test 0 
 TEST(analog_driver, test0)
 {
-    // 
+    // ADC1 
+    // 0x40000000UL + 0x00010000UL + 0x2000UL; 
+
+    // uint32_t adc1_fake = CLEAR; 
+    // ADC_TypeDef *ADC1_FAKE = (ADC_TypeDef *)((uint32_t)&adc1_fake); 
+
+    // ADC1_FAKE->DR = CLEAR; 
+
+    // ADC COMMON 
+
+    // // 
+    // adc_port_init(
+    //     ADC1, 
+    //     ADC1_COMMON, 
+    //     ADC_PCLK2_4, 
+    //     ADC_RES_8, 
+    //     ADC_EOC_EACH, 
+    //     ADC_SCAN_ENABLE, 
+    //     ADC_CONT_DISABLE, 
+    //     ADC_DMA_DISABLE, 
+    //     ADC_DDS_DISABLE, 
+    //     ADC_EOC_INT_DISABLE, 
+    //     ADC_OVR_INT_DISABLE); 
 }
