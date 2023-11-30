81c95817d35d9cb28e3be3e1b2e6a419570f4074
Sam <samueldonnelly11@gmail.com>
2023-11-29 18:13:58 -0700

    Got unit tests to build and run. Added cpputest to the library workspace.

---
+++ unit_tests/tests/devices/m8q/m8q_driver_utest.cpp
@@ -0,0 +1,32 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+	// Add your C-only include files here 
+}
+
+//==================================================
+// Notes 
+//==================================================
+
+TEST_GROUP(m8q_driver)
+{
+    // Constructor 
+    void setup()
+    {
+        // 
+    }
+
+    // Destructor 
+    void teardown()
+    {
+        // 
+    }
+}; 
+
+
+// Test 0 
+TEST(m8q_driver, test0)
+{
+    // 
+}
---
+++ unit_tests/tests/peripherals/analog/analog_driver_utest.cpp
@@ -0,0 +1,32 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+	// Add your C-only include files here 
+}
+
+//==================================================
+// Notes 
+//==================================================
+
+TEST_GROUP(analog_driver)
+{
+    // Constructor 
+    void setup()
+    {
+        // 
+    }
+
+    // Destructor 
+    void teardown()
+    {
+        // 
+    }
+}; 
+
+
+// Test 0 
+TEST(analog_driver, test0)
+{
+    // 
+}
