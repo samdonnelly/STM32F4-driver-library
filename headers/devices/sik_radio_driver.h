c23c08fdbd568f2e1255838a140b1a4f6f2778a3
Sam <samueldonnelly11@gmail.com>
2024-12-06 22:30:34 -0700

    Added files for the SiK telemetry radio.

---
+++ headers/devices/sik_radio_driver.h
@@ -0,0 +1,33 @@
+/**
+ * @file sik_radio_driver.h
+ * 
+ * @author Sam Donnelly (samueldonnelly11@gmail.com)
+ * 
+ * @brief SiK telemetry radio firmware driver interface 
+ * 
+ * @details Works for the generic SiK telemetry radio as well as the RFD900 and its 
+ *          variants. 
+ * 
+ * @version 0.1
+ * @date 2024-12-06
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef _SIK_RADIO_DRIVER_H_ 
+#define _SIK_RADIO_DRIVER_H_ 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//=======================================================================================
+// 
+//=======================================================================================
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif   // _SIK_RADIO_DRIVER_H_ 
---
+++ sources/devices/sik_radio_driver.c
@@ -0,0 +1,23 @@
+/**
+ * @file sik_radio_driver.c
+ * 
+ * @author Sam Donnelly (samueldonnelly11@gmail.com)
+ * 
+ * @brief SiK telemetry radio firmware driver 
+ * 
+ * @details Works for the generic SiK telemetry radio as well as the RFD900 and its 
+ *          variants. 
+ * 
+ * @version 0.1
+ * @date 2024-12-06
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+//=======================================================================================
+// Includes 
+
+#include "sik_radio_driver.h" 
+
+//=======================================================================================
