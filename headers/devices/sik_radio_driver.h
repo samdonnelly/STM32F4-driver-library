3d9b3675ba1b6c06012b375ea0555727e843cf1a
samdonnelly <samueldonnelly11@gmail.com>
2024-12-08 23:29:03 -0700

    Added AT/RT commands to the SiK driver.

--- headers/devices/sik_radio_driver.h
+++ headers/devices/sik_radio_driver.h
@@ -27,6 +27,89 @@ extern "C" {
 
 #include <stdint.h> 
 #include "tools.h" 
+#include "uart_comm.h" 
+
+//=======================================================================================
+
+
+//=======================================================================================
+// AT/RT command declarations 
+
+extern const char 
+// Enter AT/RT command mode 
+sik_at_enter_cmd[], 
+// - ATO - exit AT command mode 
+sik_atio_cmd[], 
+// Show radio version 
+sik_ati_cmd[], 
+sik_rti_cmd[], 
+// Show board type 
+sik_ati2_cmd[], 
+sik_rti2_cmd[], 
+// Show board frequency 
+sik_ati3_cmd[], 
+sik_rti3_cmd[], 
+// Show board version 
+sik_ati4_cmd[], 
+sik_rti4_cmd[], 
+// Show all user settable EEPROM parameters 
+sik_ati5_cmd[], 
+sik_rti5_cmd[], 
+// Display TDM timing report 
+sik_ati6_cmd[], 
+sik_rti6_cmd[], 
+// Display RSSI signal report 
+sik_ati7_cmd[], 
+sik_rti7_cmd[], 
+// Display radio parameter number ‘n’ 
+sik_atsn_cmd[], 
+sik_rtsn_cmd[], 
+// Set radio parameter number ‘n’ to ‘X’ 
+sik_atsnx_cmd[], 
+sik_rtsnx_cmd[], 
+// Reboot the radio 
+sik_atz_cmd[], 
+sik_rtz_cmd[], 
+// Write current parameters to EEPROM 
+sik_atw_cmd[], 
+sik_rtw_cmd[], 
+// Reset all parameters to factory default 
+sik_atf_cmd[], 
+sik_rtf_cmd[], 
+// Enable RSSI debug reporting 
+sik_attrssi_cmd[], 
+sik_rttrssi_cmd[], 
+// Enable TDM debug reporting 
+sik_atttdm_cmd[], 
+sik_rtttdm_cmd[], 
+// Disable debug reporting 
+sik_att_cmd[], 
+sik_rtt_cmd[]; 
+
+//=======================================================================================
+
+
+//=======================================================================================
+// Enums 
+
+typedef enum {
+    SIK_AT_PARAM_S0,    // FORMAT 
+    SIK_AT_PARAM_S1,    // SERIAL_SPEED 
+    SIK_AT_PARAM_S2,    // AIR_SPEED 
+    SIK_AT_PARAM_S3,    // NETID 
+    SIK_AT_PARAM_S4,    // TXPOWER 
+    SIK_AT_PARAM_S5,    // ECC 
+    SIK_AT_PARAM_S6,    // MAVLINK 
+    SIK_AT_PARAM_S7,    // OPPRESEND 
+    SIK_AT_PARAM_S8,    // MIN_FREQ 
+    SIK_AT_PARAM_S9,    // MAX_FREQ 
+    SIK_AT_PARAM_S10,   // NUM_CHANNELS 
+    SIK_AT_PARAM_S11,   // DUTY_CYCLE 
+    SIK_AT_PARAM_S12,   // LBT_RSSI 
+    SIK_AT_PARAM_S13,   // MANCHESTER 
+    SIK_AT_PARAM_S14,   // RTSCTS 
+    SIK_AT_PARAM_S15    // MAX_WINDOW 
+} sik_at_param_number_t; 
 
 //=======================================================================================
 
--- sources/devices/sik_radio_driver.c
+++ sources/devices/sik_radio_driver.c
@@ -26,8 +26,9 @@
 //=======================================================================================
 // Notes 
 
-// Available AT commands: 
+// Available commands: 
 // - +++ - Enter AT/RT command mode 
+// - RT - Remote AT command 
 // - ATI - show radio version 
 // - ATI2 - show board type 
 // - ATI3 - show board frequency 
@@ -71,11 +72,69 @@
 
 
 //=======================================================================================
-// 
+// AT/RT command definitions 
+
+extern const char 
+// Enter AT/RT command mode 
+sik_at_enter_cmd[] = "+++", 
+// - ATO - exit AT command mode 
+sik_atio_cmd[] = "ATO", 
+// Show radio version 
+sik_ati_cmd[] = "ATI", 
+sik_rti_cmd[] = "RTI", 
+// Show board type 
+sik_ati2_cmd[] = "ATI2", 
+sik_rti2_cmd[] = "RTI2", 
+// Show board frequency 
+sik_ati3_cmd[] = "ATI3", 
+sik_rti3_cmd[] = "RTI3", 
+// Show board version 
+sik_ati4_cmd[] = "ATI4", 
+sik_rti4_cmd[] = "RTI4", 
+// Show all user settable EEPROM parameters 
+sik_ati5_cmd[] = "ATI5", 
+sik_rti5_cmd[] = "RTI5", 
+// Display TDM timing report 
+sik_ati6_cmd[] = "ATI6", 
+sik_rti6_cmd[] = "RTI6", 
+// Display RSSI signal report 
+sik_ati7_cmd[] = "ATI7", 
+sik_rti7_cmd[] = "RTI7", 
+// Display radio parameter number ‘n’ 
+sik_atsn_cmd[] = "ATSn?", 
+sik_rtsn_cmd[] = "RTSn?", 
+// Set radio parameter number ‘n’ to ‘X’ 
+sik_atsnx_cmd[] = "ATSn=X", 
+sik_rtsnx_cmd[] = "RTSn=X", 
+// Reboot the radio 
+sik_atz_cmd[] = "ATZ", 
+sik_rtz_cmd[] = "RTZ", 
+// Write current parameters to EEPROM 
+sik_atw_cmd[] = "AT&W", 
+sik_rtw_cmd[] = "RT&W", 
+// Reset all parameters to factory default 
+sik_atf_cmd[] = "AT&F", 
+sik_rtf_cmd[] = "RT&F", 
+// Enable RSSI debug reporting 
+sik_attrssi_cmd[] = "AT&T=RSSI", 
+sik_rttrssi_cmd[] = "RT&T=RSSI", 
+// Enable TDM debug reporting 
+sik_atttdm_cmd[] = "AT&T=TDM", 
+sik_rtttdm_cmd[] = "RT&T=TDM", 
+// Disable debug reporting 
+sik_att_cmd[] = "AT&T", 
+sik_rtt_cmd[] = "RT&T"; 
+
+//=======================================================================================
+
+
+//=======================================================================================
+// Global data 
 
 // Driver data record 
 typedef struct sik_driver_data_s 
 {
+    USART_TypeDef *uart; 
     uint8_t at_mode : 1; 
 }
 sik_driver_data_t; 
@@ -90,8 +149,14 @@ static sik_driver_data_t sik_driver_data;
 // Initialization 
 
 // Initialization 
-void sik_init(void)
+void sik_init(USART_TypeDef *uart)
 {
+    if (uart == NULL)
+    {
+        return; 
+    }
+    
+    sik_driver_data.uart = uart; 
     sik_driver_data.at_mode = CLEAR_BIT; 
 }
 
@@ -120,6 +185,40 @@ void sik_at_mode(void)
     // There should be some function to check for the "OK" from the radio when requesting 
     // AT command mode which then sets this bit if it's seen. 
     sik_driver_data.at_mode = SET_BIT; 
+}
+
+
+// Send command 
+void sik_at_send_cmd(const char *cmd)
+{
+    if (cmd == NULL)
+    {
+        return; 
+    }
+
+    uart_sendstring(sik_driver_data.uart, cmd); 
+}
+
+
+// 
+void sik_at_param_get(sik_at_param_number_t param)
+{
+    // Format the command 
+    char format_cmd[50]; 
+
+    sik_at_send_cmd(format_cmd); 
+}
+
+
+// 
+void sik_at_param_set(
+    sik_at_param_number_t param, 
+    uint32_t value)
+{
+    // Format the command 
+    char format_cmd[50]; 
+
+    sik_at_send_cmd(format_cmd); 
 }
 
 //=======================================================================================
