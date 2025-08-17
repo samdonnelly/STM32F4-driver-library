344701efe57b8692b1b62ad730cad769a55a17e1
Sam <samueldonnelly11@gmail.com>
2025-08-16 21:41:31 -0600

    Updated formatting of Madgwick filter code.

--- tools/madgwick_filter.h
+++ tools/madgwick_filter.h
@@ -29,8 +29,11 @@ public:
     
     /**
      * @brief Constructor 
+     * 
+     * @param beta_setpoint 
+     * @param sample_period 
      */
-    MadgwickFilter() = default;
+    MadgwickFilter(float beta_setpoint, float sample_period);
 
     /**
      * @brief Destructor 
@@ -47,10 +50,55 @@ public:
 
     /**
      * @brief Madgwick filter implementation 
+     * 
+     * @param w_x 
+     * @param w_y 
+     * @param w_z 
+     * @param a_x 
+     * @param a_y 
+     * @param a_z 
+     * @param m_x 
+     * @param m_y 
+     * @param m_z 
      */
     void Madgwick(float w_x, float w_y, float w_z, float a_x, float a_y, float a_z, float m_x, float m_y, float m_z);
 
+    /**
+     * @brief Get Roll to NED frame 
+     * 
+     * @return float : roll (degrees) 
+     */
+    float GetRoll(void); 
+
+    /**
+     * @brief Get Pitch to NED frame 
+     * 
+     * @return float : pitch (degrees) 
+     */
+    float GetPitch(void); 
+
+    /**
+     * @brief Get Yaw to NED frame 
+     * 
+     * @return float : yaw (degrees) 
+     */
+    float GetYaw(void); 
+
 private: 
+
+    /**
+     * @brief Inverse square root calculation 
+     * 
+     * @param x : value to inverse square root 
+     * @return float : result of calculation 
+     */
+    float invSqrt(float x);
+
+    // Data 
+    float beta;				  // Algorithm gain 
+    float inv_sample_freq;    // Inverse sample frequency (1 / sample_frequency (Hz) == dt) 
+    float q0, q1, q2, q3;	  // quaternion of sensor frame relative to auxiliary frame 
+    float roll, pitch, yaw;   // Orientation 
 };
 
 //=======================================================================================
